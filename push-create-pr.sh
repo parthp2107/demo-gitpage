3318c04a5ff34f65a9188cec2e8100d7772a6c66
parthp2107 <83201188+parthp2107@users.noreply.github.com>
2021-11-24 18:38:29 +0530

    Create push-create-pr.sh

---
+++ push-create-pr.sh
@@ -0,0 +1,43 @@
+   
+#!/bin/sh -l
+
+git_setup() {
+  cat <<- EOF > $HOME/.netrc
+		machine github.com
+		login $GITHUB_ACTOR
+		password $GITHUB_TOKEN
+		machine api.github.com
+		login $GITHUB_ACTOR
+		password $GITHUB_TOKEN
+EOF
+  chmod 600 $HOME/.netrc
+
+  git config --global user.email "$GITBOT_EMAIL"
+  git config --global user.name "$GITHUB_ACTOR"
+}
+
+git_cmd() {
+  if [[ "${DRY_RUN:-false}" == "true" ]]; then
+    echo $@
+  else
+    eval $@
+  fi
+}
+
+PR_BRANCH="auto-$INPUT_PR_BRANCH-$GITHUB_SHA"
+MESSAGE=$(git log -1 $GITHUB_SHA | grep "AUTO" | wc -l)
+
+if [[ $MESSAGE -gt 0 ]]; then
+  echo "Autocommit, NO ACTION"
+  exit 0
+fi
+
+PR_TITLE=$(git log -1 --format="%s" $GITHUB_SHA)
+
+git_setup
+git_cmd git remote update
+git_cmd git fetch --all
+git_cmd git checkout -b "${PR_BRANCH}" origin/"${INPUT_PR_BRANCH}"
+git_cmd git cherry-pick "${GITHUB_SHA}"
+git_cmd git push -u origin "${PR_BRANCH}"
+git_cmd hub pull-request -b "${INPUT_PR_BRANCH}" -h "${PR_BRANCH}" -l "${INPUT_PR_LABELS}" -m "\"AUTO: ${PR_TITLE}\""
